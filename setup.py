ed478f3296a3786e13d19144990cab9daad3770e
Aaron M Hosford <hosford42@gmail.com>
2018-05-27 13:57:43 -0500

    Improved setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,48 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+import traceback
+
+
+def get_long_description():
+    try:
+        with open('README.md', encoding='utf-8') as file:
+            return file.read()
+    except OSError:
+        traceback.print_exc()
+        print("Long description will not be included.")
+        return None
+
 
 setup(
     name='Chatty',
     version='0.0',
-    packages=['chatty'],
+    packages=find_packages(),
     url='https://github.com/hosford42/Chatty',
+    download_url='https://pypi.org/project/Chatty',
     license='MIT',
     author='Aaron Hosford',
     author_email='hosford42@gmail.com',
-    description='Chat bot framework',
+    description='A multi-platform chat bot framework',
+    long_description=get_long_description(),
+    keywords='chat bot chatbot AI artificial intelligence email xmpp IM message text messaging framework slack session '
+             'conversation interactive platform multi-platform window local remote protocol',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Telecommunications Industry',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Topic :: Communications :: Chat',
+        'Topic :: Communications :: Email',
+        'Topic :: Internet :: XMPP',
+        'Topic :: Multimedia',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Topic :: Scientific/Engineering :: Human Machine Interfaces',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+        'Topic :: Software Development :: User Interfaces',
+        'Topic :: Text Processing :: Linguistic'
+    ],
     install_requires=[
         'tzlocal',  # MIT
     ],
