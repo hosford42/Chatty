7d53a339c047288f9b8869d97151f72b410dce47
Aaron M Hosford <hosford42@gmail.com>
2018-05-26 23:48:19 -0500

    Added URL to setup config

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='Chatty',
     version='0.0',
     packages=['chatty'],
-    url='',
+    url='https://github.com/hosford42/Chatty',
     license='MIT',
     author='Aaron Hosford',
     author_email='hosford42@gmail.com',
@@ -14,7 +14,7 @@ setup(
     ],
     extras_require={
         'slack': ['slackclient>=1.2'],
-        'tkinter': ['tkinter>=8.6'],
+        'tkinter': ['tkinter>=8.6'],  # This doesn't come as a built-in package for all platforms.
         'xmpp': ['sleekxmpp>=1.3', 'dnspython>=1.15'],
     }
 )
